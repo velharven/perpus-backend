03f6320f99cf3a6cbc72a3a07a077f287260d8e3
Vel Harven <velharven@gmail.com>
2026-01-28 03:58:39 +0700

    update menyesuaikan dengan file .env

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 app.get("/api/buku", async (req, res) => {
